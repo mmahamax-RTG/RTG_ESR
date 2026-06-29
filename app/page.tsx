6049a74ef3a88fe4cf9ec93fcc4417e2a804ac69
mmahamax-RTG <mmahamax@gmail.com>
2026-06-29 18:05:59 +0800

    Update page2.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -521,9 +521,10 @@ if (!role) {
             updated = updated.filter(name => name !== staff.name);
           }
 
+
 const selectedStaff = updated.map(name =>
-  staffData[form.team].find(s => s.name === name)
-);
+  staffData[form.team as keyof typeof staffData]?.find(s => s.name === name)
+).filter(Boolean);
 
 const groups = [
   ...new Set(
